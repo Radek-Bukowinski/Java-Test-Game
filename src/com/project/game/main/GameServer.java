05a5e4a082179efea983b6ebfb4fcc0c9126a152
Presisor <74270526+Radek-Bukowinski@users.noreply.github.com>
2021-07-07 12:01:56 +0100

    Update GameServer.java

--- src/com/project/game/main/GameServer.java
+++ src/com/project/game/main/GameServer.java
@@ -3,7 +3,12 @@ import java.net.*;
 
 public class GameServer {
   private ServerSocket serverSocket;
+  private int falsePlayerCount;
   private int playerCount;
+  private ServerSideConnection player1;
+  private ServerSideConnection player2;
+  private ServerSideConnection player3;
+  private ServerSideConnection player4;
   
   public GameServer(){
     playerCount = 0;
@@ -21,6 +26,22 @@ public class GameServer {
       while(playerCount < 4){
         Socket socket = serverSocket.accept();
         playerCount++;
+        ServerSideConnection serverSideConnection = new ServerSideConnection(socket, playerCount);
+        if(playerCount == 1){
+            player1 = serverSideConnection;
+        }
+        else if(playerCount == 2){
+            player2 = serverSideConnection;
+        }
+        else if(playerCount == 3){
+            player3 = serverSideConnection;
+        }
+        else if(playerCount == 4){
+            player4 = serverSideConnection;
+        }
+        
+        Thread thread = new Thread(serverSideConnection);
+        thread.start();
       }
     } catch(IOException ioException){
       ioException.printStackTrace();
@@ -28,8 +49,32 @@ public class GameServer {
     }
   }
   
-  private class ServerSideConnection(){
-  
+  private class ServerSideConnection() implements Runnable{
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private int playerID;
+    
+    public ServerSideConnection(Socket socketIn, int id){
+        socket = socketIn;
+        playerID = id;
+      
+      try{
+          dataInputStream = new DataInputStream(socket.getInputStream());
+          dataOutputStream = new DataOutputStream(socket.getOutputStream());
+      } catch (IOException ioException) {
+          ioException.printStackTrace();
+      }
+      
+      public void run(){
+        try {
+          dataOutputStream.writeInt(playerID);
+          dataOutputStream.flush();
+        } catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+      }
+    }
   }
   
   public static void main(String[] args){
