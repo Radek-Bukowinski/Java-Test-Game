77c15bd24fdf40d7e012eac20c3a2e034fda7cee
Presisor <radekbukowinski@protonmail.com>
2021-10-31 22:35:28 +0000

    Added texture + collision

--- src/com/project/game/objects/Enemy.java
+++ src/com/project/game/objects/Enemy.java
@@ -1,16 +1,27 @@
 package com.project.game.objects;
 
 import com.project.game.identifiers.ID;
+import com.project.game.identifiers.STATE;
+import com.project.game.main.BufferedImageLoader;
+import com.project.game.main.Game;
 import com.project.game.main.GameObject;
 import com.project.game.main.RendererHandler;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Random;
 
 public class Enemy extends GameObject {
     private RendererHandler renderer;
 
+    private BufferedImageLoader bufferedImageLoader= new BufferedImageLoader();
+    private BufferedImage bufferedImage;
+
+    private Random random = new Random();
 
     private GameObject PLAYER = null;
+
+
 
     private float directionX;
     private float directionY;
@@ -47,15 +58,20 @@ public class Enemy extends GameObject {
         this.renderer = renderer;
         velocityY = 1;
         velocityX = 1;
+        bufferedImage = bufferedImageLoader.loadImage("/enemy.png");
     }
 
     @Override
     public void tick() {
-        x += vectorX;
-        y += vectorY;
+        x += velocityX;
+        y += velocityY;
 
+        collision();
 
+        //vectorX = Game.getRandomNumber(-5, 5);
+        //vectorY = Game.getRandomNumber(-5, 5);
 
+        /*
         if(PLAYER != null) {
 
             float differenceX = (float) x - PLAYER.getX() - 16;
@@ -76,8 +92,10 @@ public class Enemy extends GameObject {
             getPlayer();
         }
 
-       vectorX = (attractionX + directionX) / 2;
-       vectorY = (attractionY + directionY) / 2;
+         */
+
+       //vectorX = (attractionX + directionX) / 2;
+       //vectorY = (attractionY + directionY) / 2;
 
         //x += velocityX;
         //y += velocityY;
@@ -94,11 +112,86 @@ public class Enemy extends GameObject {
 
     }
 
+    private void collision() {
+        if(Game.windowSTATE == STATE.Game) {
+            if(ENEMY != null) {
+                //System.out.println("PLAYER FOUND");
+                for (int i = 0; i < renderer.objects.size(); i++) {
+                    GameObject temporaryObject = renderer.objects.get(i);
+                    if (temporaryObject.getId() == ID.Block || temporaryObject.getId() == ID.Crate || temporaryObject.getId() == ID.Coin) {
+                        //System.out.println("BLOCK FOUND");
+                        if (ENEMY.getBounds().intersects(temporaryObject.getBounds())) {
+                            //System.out.println("collision detected");
+                            if(horizontalCollision().intersects(temporaryObject.getBounds())){
+                                if(velocityX > 0){
+                                    velocityX *= -1;
+                                    //x = temporaryObject.getX() - 32;
+                                }else if(velocityX < 0){
+                                    velocityX *= -1;
+                                    //x = temporaryObject.getX() + 32;
+                                }
+                            }
+
+                            if(verticalCollision().intersects(temporaryObject.getBounds())){
+                                if(velocityY > 0){
+                                    velocityY *= -1;
+                                    //y = temporaryObject.getY() - 32;
+                                }else if(velocityY < 0){
+                                    velocityY *= -1;
+                                    //y = temporaryObject.getY() + 32;
+                                }
+                            }
+                        }
+
+                        //break;
+                    }
+
+                }
+            } else{
+                getEnemy();
+            }
+
+            /*
+            if (PLAYER != null) {
+
+                if (ENEMY != null) {
+                    if (PLAYER.getBounds().intersects(ENEMY.getBounds())) {
+                        PLAYER.setHealth(PLAYER.getHealth() - 2);
+                    }
+                }else{
+                    getEnemy();
+                }
+
+             */
+
+
+        }
+    }
+
+    public Rectangle horizontalCollision(){
+        float boundaryX = x + velocityX;
+        float boundaryY = y + 2;
+        float boundaryWidth = 16 + velocityX / 2;
+        float boundaryHeight = 12;
+
+        return new Rectangle((int) boundaryX, (int) boundaryY, (int) boundaryWidth, (int) boundaryHeight);
+    }
+
+    public Rectangle verticalCollision(){
+        float boundaryX = x + 2;
+        float boundaryY = y + velocityY;
+        float boundaryWidth = 12;
+        float boundaryHeight = 16 + velocityY / 2;
+
+        return new Rectangle((int) boundaryX, (int) boundaryY, (int) boundaryWidth, (int) boundaryHeight);
+    }
+
     @Override
     public void render(Graphics graphics) {
         if(ENEMY != null) {
             graphics.setColor(Color.RED);
-            graphics.fillRect((int) x, (int) y, 16, 16);
+            //graphics.fillRect((int) x, (int) y, 16, 16);
+            graphics.drawImage(bufferedImage, (int) x, (int) y, null);
         }else{
             getEnemy();
         }
