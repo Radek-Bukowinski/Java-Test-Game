c181f1ec5ef2211f9d4ec3a291c6c198afa8f09b
radekbukowinski <radekbukowinski@protonmail.com>
2022-02-02 08:23:27 +0000

    Code cleanup + comments

--- src/com/project/game/objects/Player.java
+++ src/com/project/game/objects/Player.java
@@ -14,15 +14,18 @@ import java.util.Random;
 
 import static com.project.game.main.Spawner.enemyCount;
 
+// Player inherits the abstract the GameObject class.
 public class Player extends GameObject {
+
+    // Declaring instances of classes we use in this class.
     private RendererHandler renderer;
-    private Random random = new Random();
     private Game game;
     private BufferedImageLoader bufferedImageLoader = new BufferedImageLoader();
     private BufferedImage bufferedImage;
     private HUD hud;
     private ClientSideConnection clientSideConnection;
 
+    // The constructor used to create a new Player object.
     public Player(int x, int y, int health, ID id, RendererHandler renderer, Game game, boolean isCollidible) {
         super(x, y, health, id, isCollidible);
         this.renderer = renderer;
@@ -32,17 +35,25 @@ public class Player extends GameObject {
 
     @Override
     public void tick() {
+        // We update the position of the player (x and y) based on the velocity, which is what changes on key input.
         x += velocityX;
         y += velocityY;
 
+        // Update health
         HUD.health = health;
 
+        // Update other values
         collision();
         isAlive();
         spawn();
     }
 
     private void spawn() {
+        /*
+            Here we handle the spawning of enemies
+            This is called recursively in the tick() function
+            If there is less than five enemies, we spawn an enemy inside of one 4 predetermined positions around the player
+        */
         if (renderer.enemies.size() < 5) {
             int chosen = Game.getRandomNumber(1, 4);
             if(chosen == 1){
@@ -64,9 +75,12 @@ public class Player extends GameObject {
             for (int i = 0; i < renderer.collidibles.size(); i++) {
                 // Search through objects that are collidible (solid objects only)
                 GameObject temporaryObject = renderer.collidibles.get(i);
+                    // Check if the collidible object bounds are within the bounds of the player
+                    // Bounds are calculated from the x and y of the object, plus its size
                     if (this.getBounds().intersects(temporaryObject.getBounds())) {
-                        //System.out.println("collision detected");
+                        //Handle collision if it is horizontal
                         if (horizontalCollision().intersects(temporaryObject.getBounds())) {
+                            // We set the velocity in that direction to 0, because we now know it has collided.
                             if (velocityX > 0) {
                                 velocityX = 0;
                                 x = temporaryObject.getX() - 32;
@@ -75,8 +89,9 @@ public class Player extends GameObject {
                                 x = temporaryObject.getX() + 32;
                             }
                         }
-
+                        // Handle collision if it is vertical
                         if (verticalCollision().intersects(temporaryObject.getBounds())) {
+                            // We set the velocity in that direction to 0, because we now know it has collided.
                             if (velocityY > 0) {
                                 velocityY = 0;
                                 y = temporaryObject.getY() - 32;
@@ -92,6 +107,7 @@ public class Player extends GameObject {
         for (int i = 0; i < renderer.objects.size(); i++) {
                 GameObject temporaryObject = renderer.objects.get(i);
                 if (renderer.objects.get(i).getId() == ID.Coin) {
+                    //If the player has collided with a coin, remove it from being rendered, and add 100 to the players score.
                     if (this.getBounds().intersects(temporaryObject.getBounds())) {
                         renderer.removeObject(temporaryObject);
                         hud.score += 100;
@@ -99,94 +115,18 @@ public class Player extends GameObject {
 
                 }
                 if (renderer.objects.get(i).getId() == ID.Health) {
+                    //If the player has collided with a health pack, remove it from being rendered, and add 10 health to the player.
                     if (this.getBounds().intersects(temporaryObject.getBounds())) {
                         renderer.removeObject(temporaryObject);
-                        hud.health += 10;
+                        if(hud.health == 100){
+                            return;
+                        }else {
+                            hud.health += 10;
+                        }
                     }
 
                 }
-            }
-
-
-        /*
-        if(game.windowSTATE == STATE.Game) {
-            if(PLAYER != null) {
-                //System.out.println("PLAYER FOUND");
-                for (int i = 0; i < renderer.objects.size(); i++) {
-                    GameObject temporaryObject = renderer.objects.get(i);
-                    if (temporaryObject.getId() == ID.Block || temporaryObject.getId() == ID.Crate) {
-                        //System.out.println("BLOCK FOUND");
-                        if (game.PLAYER.getBounds().intersects(temporaryObject.getBounds())) {
-                            System.out.println("collision detected");
-                            if(horizontalCollision().intersects(temporaryObject.getBounds())){
-                                if(velocityX > 0){
-                                    velocityX = 0;
-                                    x = temporaryObject.getX() - 32;
-                                }else if(velocityX < 0){
-                                    velocityX = 0;
-                                    x = temporaryObject.getX() + 32;
-                                }
-                            }
-
-                            if(verticalCollision().intersects(temporaryObject.getBounds())){
-                                if(velocityY > 0){
-                                    velocityY = 0;
-                                    y = temporaryObject.getY() - 32;
-                                }else if(velocityY < 0){
-                                    velocityY = 0;
-                                    y = temporaryObject.getY() + 32;
-                                }
-                            }
-                        }
-
-                        //break;
-                    }
-
-                }
-            } else{
-                getPlayer();
-            }
-
-
-
-
-            if (PLAYER != null) {
-
-                    for (int i = 0; i < renderer.objects.size(); i++) {
-                        GameObject temporaryObject = renderer.objects.get(i);
-                        if (renderer.objects.get(i).getId() == ID.Coin) {
-                            if (PLAYER.getBounds().intersects(temporaryObject.getBounds())) {
-                                renderer.removeObject(temporaryObject);
-                                hud.score += 100;
-                            }
-
-                        }
-                        if (renderer.objects.get(i).getId() == ID.Health) {
-                            if (PLAYER.getBounds().intersects(temporaryObject.getBounds())) {
-                                renderer.removeObject(temporaryObject);
-                                hud.health += 10;
-                            }
-
-                        }
-                }
-
-                if (ENEMY != null) {
-                    if (PLAYER.getBounds().intersects(ENEMY.getBounds())) {
-                        PLAYER.setHealth(PLAYER.getHealth() - 2);
-                    }
-                } else {
-                    getEnemy();
-                }
-
-
-            } else{
-                getPlayer();
-            }
-            */
-
-
-
-
+        }
     }
 
 
@@ -209,12 +149,16 @@ public class Player extends GameObject {
     }
 
     private void isAlive() {
+        // Called in a loop in the tick(), function to ensure that the player has not died.
+        // If the player has died, we reset the game.
         if(this.getHealth() == 0) {
             Game.windowSTATE = STATE.Death;
         }
     }
     
     public void connectToServer(){
+        // Create a new connection on the clients side.
+        // This is not used for hosting, only for connecting to servers.
         clientSideConnection = new ClientSideConnection();
     }
     
@@ -227,7 +171,7 @@ public class Player extends GameObject {
             try{
                 socket = new Socket("localhost", 51734);
                 dataInputStream = new DataInputStream(socket.getInputStream());
-                dataOutputStream =new DataOutputStream(socket.getOutputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
             } catch(IOException ioException){
                 ioException.printStackTrace();
               }
@@ -236,8 +180,7 @@ public class Player extends GameObject {
 
     @Override
     public void render(Graphics graphics) {
-        graphics.setColor(Color.white);
-        //graphics.fillRect((int)x, (int)y, 32, 32);
+        // Draw the player sprite, fetch relevant image to use as sprite from the constructor.
         graphics.drawImage(bufferedImage, (int) x, (int) y, null);
     }
 
